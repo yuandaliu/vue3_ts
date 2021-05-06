f524e2d9816a9475e4600a7617427d8a0bbe22a2
yuandaliu <48154935+yuandaliu@users.noreply.github.com>
2021-05-06 11:18:49 +0800

    整理页面内容

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,16 +3,17 @@ import Home from '../views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
+    path: '/home',
     name: 'Home',
     component: Home
   },
   {
+    path: '/',
+    redirect: '/home'
+  },
+  {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Demo msg="This is demo page!"/>
   </div>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import Demo from '@/components/Demo.vue'
+
+@Options({
+  components: {
+    Demo
+  }
+})
+export default class Home extends Vue {
+  // const state: any ={
+  //   data: 0
+  // }
+}
+</script>
